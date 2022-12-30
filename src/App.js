8b3c72af8ee50950f14b842f4066870fa80e81fe
T-Cee <81631627+tochikaze3@users.noreply.github.com>
2022-12-30 20:15:11 +0000

    still editing section2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Section1 from './sections/Section1';
+import Section2 from './sections/Section2';
 
 
 function App() {
   return (
     <div className="App">
       <Section1 />
+      <Section2/>
     </div>
   );
 }
---
+++ src/sections/Section2.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Section2.css'
+function Section2() {
+  return (
+    <div className='Section2'>
+        
+        <div className='nav---left'>
+        <h3>About</h3>
+        </div>
+        <div className="hello">
+           
+            <p>I am excited about the opportunity to use my passion for digital design <br/> to contribute to the development and growth of innovative projects. <br/>Solving problems through creative and user-centered design is what really gets me energized and enthusiastic.</p>
+            
+        </div>
+        <div className="nav---right">
+            <ul>
+                <li>Work</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div> 
+    </div>
+    
+  )
+}
+
+export default Section2
